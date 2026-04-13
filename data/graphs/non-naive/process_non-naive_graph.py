dc011b46237de727e22feea612bb92df1eead522
vereesmort <kkatcchya@gmail.com>
2026-04-13 11:16:28 +0200

    replaced both .append() calls with a single pd.concat(), and removed the empty DataFrame initialisation and del statements.

--- data/graphs/non-naive/process_non-naive_graph.py
+++ data/graphs/non-naive/process_non-naive_graph.py
@@ -1,22 +1,16 @@
 import pandas as pd
 
-# Create storage dataframe
-out_edges = pd.DataFrame(columns=['head', 'relation', 'tail'])
-
 # Load core graph (drug-target and PPI edges)
 core = pd.read_csv('../../processed/core_network_ppi_drugtarget.tsv', header=None, sep='\t', dtype={0:str, 1:str, 2:str})
 core.columns = ['head', 'relation', 'tail']
 core.drop_duplicates(inplace=True)
-out_edges = out_edges.append(core)
-del core
-
 
 # Add Polypharmic side effect edges
 polySE = pd.read_csv('../../processed/polypharmacy/train_polypharmacy.tsv', header=None, sep='\t')
 polySE.columns = ['head', 'relation', 'tail']
 polySE.drop_duplicates(inplace=True)
-out_edges = out_edges.append(polySE, ignore_index=True)
-del polySE
+
+out_edges = pd.concat([core, polySE], ignore_index=True)
 
 # Save edges
 out_edges.to_csv('edgelist_non-naive.tsv', sep='\t', header=None, index=False)
