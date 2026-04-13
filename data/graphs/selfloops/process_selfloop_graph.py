1f14458e9a696078e32425a8e5406531bbf45724
vereesmort <kkatcchya@gmail.com>
2026-04-13 11:03:56 +0200

    .append() calls are replaced with a single pd.concat() at the end

--- data/graphs/selfloops/process_selfloop_graph.py
+++ data/graphs/selfloops/process_selfloop_graph.py
@@ -1,14 +1,9 @@
 import pandas as pd
 
-# Create storage dataframe
-out_edges = pd.DataFrame(columns=['head', 'relation', 'tail'])
-
 # Load core graph (drug-target and PPI edges)
 core = pd.read_csv('../../processed/core_network_ppi_drugtarget.tsv', header=None, sep='\t', dtype={0:str, 1:str, 2:str})
 core.columns = ['head', 'relation', 'tail']
 core.drop_duplicates(inplace=True)
-out_edges = out_edges.append(core)
-del core
 
 # Add monopharmic side effect edges as drug self-loops
 monoSE = pd.read_csv('../../processed/monopharmacy_edges.tsv', header=None, sep='\t')
@@ -16,15 +11,13 @@ monoSE.columns = ['head', 'relation', 'tail']
 monoSE['relation'] = monoSE['tail']  # Side effect type becomes the relation
 monoSE['tail'] = monoSE['head']  # Tail becomes same node as head
 monoSE.drop_duplicates(inplace=True)
-out_edges = out_edges.append(monoSE, ignore_index=True)
-del monoSE
 
 # Add Polypharmic side effect edges
 polySE = pd.read_csv('../../processed/polypharmacy/train_polypharmacy.tsv', header=None, sep='\t')
 polySE.columns = ['head', 'relation', 'tail']
 polySE.drop_duplicates(inplace=True)
-out_edges = out_edges.append(polySE, ignore_index=True)
-del polySE
+
+out_edges = pd.concat([core, monoSE, polySE], ignore_index=True)
 
 # Save edges
 out_edges.to_csv('edgelist_selfloops.tsv', sep='\t', header=None, index=False)
